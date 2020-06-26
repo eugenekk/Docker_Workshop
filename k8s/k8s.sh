18688da4b6085a3e474bc761a7ea1d6e901955c1
eugenekk <08youjin12@gmail.com>
2020-06-26 16:04:50 +0900

    Workshop_3

---
+++ k8s/k8s.sh
@@ -0,0 +1,6 @@
+# Image Deploy
+kubectl run myworkshop --image=08youjin12/docker_workshop --port=80
+# Pods 20
+kubectl.exe scale deployment myworkshop --replicas=20
+# Service Port 80
+kubectl.exe expose deployment/myworkshop --type="NodePort" --port 80
